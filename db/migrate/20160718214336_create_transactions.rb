541b2099e050741190cc662ba4e43963e817a507
Christopher Soden <karavol27@gmail.com>
2016-07-18 16:06:46 -0600

    Add data import tasks

---
+++ db/migrate/20160718214336_create_transactions.rb
@@ -0,0 +1,12 @@
+class CreateTransactions < ActiveRecord::Migration[5.0]
+  def change
+    create_table :transactions do |t|
+      t.references :invoice, foreign_key: true
+      t.integer :credit_card_number
+      t.datetime :credit_card_expiration_date
+      t.string :result
+      t.datetime :created_at
+      t.datetime :updated_at
+    end
+  end
+end
