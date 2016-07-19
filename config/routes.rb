28ed153ab6056c1cb1d17b924881a985bb933418
Robbie Jaeger <jaeger.rob@gmail.com>
2016-07-19 16:13:21 -0600

    Add invoice transactions endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -42,7 +42,9 @@ Rails.application.routes.draw do
       resources :merchants, only: [:index, :show]
       resources :transactions, only: [:index, :show]
       resources :customers, only: [:index, :show]
-      resources :invoices, only: [:index, :show]
+      resources :invoices, only: [:index, :show] do
+        get 'transactions', to: 'invoices/transactions#index'
+      end
       resources :invoice_items, only: [:index, :show]
       resources :items, only: [:index, :show]
     end
