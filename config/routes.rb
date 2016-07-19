bbba960e995b278d9919d28500ab65592629aafc
Christopher Soden <karavol27@gmail.com>
2016-07-18 19:35:39 -0600

    Invoice item index and show working

--- config/routes.rb
+++ config/routes.rb
@@ -6,11 +6,7 @@ Rails.application.routes.draw do
       namespace :v1 do
         resources :merchants, except: [:new, :edit], defaults: {format: :json}
         resources :invoices, except: [:new, :update], defaults: {format: :json}
+        resources :invoice_items, except: [:new, :update], defaults: {format: :json}
       end
     end
   end
-
-
-
-
-  # get 'invoices/index', to: 'invoices#index', as: :invoices
