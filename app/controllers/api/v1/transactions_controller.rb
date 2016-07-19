e319cb290568debe8f7b3cbc19a9d8abefc6cf96
Robbie Jaeger <jaeger.rob@gmail.com>
2016-07-19 14:53:58 -0600

    Add jbuilder for transactions, customers, and merchants

--- app/controllers/api/v1/transactions_controller.rb
+++ app/controllers/api/v1/transactions_controller.rb
@@ -1,10 +1,10 @@
 class Api::V1::TransactionsController < ApplicationController
 
   def index
-    render json: Transaction.all
+    @transaction = Transaction.all
   end
 
   def show
-    render json: Transaction.find(params[:id])
+    @transactions = Transaction.find(params[:id])
   end
 end
