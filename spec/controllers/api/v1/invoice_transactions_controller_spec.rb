86dd5730d4c485994051d4f09a5b20a961e26e0b
Robbie Jaeger <jaeger.rob@gmail.com>
2016-07-20 10:54:00 -0600

    Add invoice transactions relationship test

--- spec/controllers/api/v1/invoice_transactions_controller_spec.rb
+++ spec/controllers/api/v1/invoice_transactions_controller_spec.rb
@@ -1,20 +1,22 @@
 require 'rails_helper'
 
-RSpec.describe Api::V1::Invoices::TransactionsController, type: :controller do
+RSpec.describe Api::V1::Invoices::InvoiceItemsController, type: :controller do
   render_views
 
-  describe "returns transactions for an invoice" do
-    it 'returns list of all transactions' do
-      transaction_1 = create(:transaction)
-      transaction_2 = create(:transaction, result: "failure")
+  describe "returns invoice items for an invoice" do
+    it 'returns list of all invoice items' do
+      invoice_item_1 = create(:invoice_item)
+      invoice_item_2 = create(:invoice_item)
 
-      get :index, params: {invoice_id: transaction_1.invoice.id, format: :json}
+      get :index, params: {invoice_id: invoice_item_1.invoice.id, format: :json}
       result = JSON.parse(response.body)
 
       expect(result.count).to eq(1)
-      expect(result[0]["id"]).to eq(transaction_1.id)
-      expect(result[0]["credit_card_number"]).to eq(transaction_1.credit_card_number)
-      expect(result[0]["result"]).to eq(transaction_1.result)
+      expect(result[0]["id"]).to eq(invoice_item_1.id)
+      expect(result[0]["item_id"]).to eq(invoice_item_1.item_id)
+      expect(result[0]["invoice_id"]).to eq(invoice_item_1.invoice_id)
+      expect(result[0]["quantity"]).to eq(invoice_item_1.quantity)
+      expect(result[0]["unit_price"]).to eq(invoice_item_1.unit_price)
     end
   end
 end
