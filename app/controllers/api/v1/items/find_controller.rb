c2001a6a47b84aea4c2083a2587cfc7e0db1e96a
Robbie Jaeger <jaeger.rob@gmail.com>
2016-07-19 21:41:55 -0600

    Add case insensitive where and find_by

--- app/controllers/api/v1/items/find_controller.rb
+++ app/controllers/api/v1/items/find_controller.rb
@@ -1,11 +1,11 @@
 class Api::V1::Items::FindController < ApplicationController
 
   def show
-    @item = Item.find_by(item_params)
+    @item = Item.case_insensitive_find_by(item_params)
   end
 
   def index
-    @items = Item.where(item_params)
+    @items = Item.case_insensitive_where(item_params)
   end
 
   private
--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,19 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+  def self.case_insensitive_where(params)
+    if ["name", "description", "first_name", "last_name", "status", "result"].include?(params.keys[0])
+      where("LOWER(#{params.keys[0]}) LIKE ?", params.values[0].downcase)
+    else
+      where(params)
+    end
+  end
+
+  def self.case_insensitive_find_by(params)
+    if ["name", "description", "first_name", "last_name", "status", "result"].include?(params.keys[0])
+      find_by("LOWER(#{params.keys[0]}) LIKE ?", params.values[0].downcase)
+    else
+      find_by(params)
+    end
+  end
 end
