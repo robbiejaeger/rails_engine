921e0de3f6ef7160905f3aee4fdcccfc9725c094
Christopher Soden <karavol27@gmail.com>
2016-07-20 19:13:46 -0600

    Favorite Customer functionality implemented

--- config/routes.rb
+++ config/routes.rb
@@ -43,7 +43,7 @@ Rails.application.routes.draw do
         get 'items', to: 'merchants/items#index'
         get 'invoices', to: 'merchants/invoices#index'
         get 'customers_with_pending_invoices', to: 'merchants/customers_with_pending_invoices#index'
-        get 'favorite_customer', to: 'merchants/favorite_customer#index'
+        get 'favorite_customer', to: 'merchants/favorite_customer#show'
       end
 
       resources :transactions, only: [:index, :show] do
